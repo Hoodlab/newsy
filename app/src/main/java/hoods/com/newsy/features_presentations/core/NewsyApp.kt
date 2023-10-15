f3e1b1e263098d6b5e13e9e79b38893b2ca6c2ef
hood <hoods.tech@gmail.com>
2023-10-15 12:27:36 +0300

    Discover Feature implementation

--- app/src/main/java/hoods/com/newsy/features_presentations/core/NewsyApp.kt
+++ app/src/main/java/hoods/com/newsy/features_presentations/core/NewsyApp.kt
@@ -1,10 +1,19 @@
 package hoods.com.newsy.features_presentations.core
 
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import hoods.com.newsy.features_presentations.core.components.NewysAppDrawerContent
 import hoods.com.newsy.features_presentations.core.navigation.NewsyNavigationActions
 import hoods.com.newsy.features_presentations.core.navigation.NewsyNavigationGraph
+import hoods.com.newsy.features_presentations.core.navigation.UiScreen
+import kotlinx.coroutines.launch
 
 @Composable
 fun NewsyApp() {
@@ -12,10 +21,37 @@ fun NewsyApp() {
     val navActions = remember {
         NewsyNavigationActions(navController)
     }
+    val drawerState = rememberDrawerState(DrawerValue.Closed)
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentRoute =
+        navBackStackEntry?.destination?.route ?: UiScreen.HomeScreen().route
+    val scope = rememberCoroutineScope()
+    ModalNavigationDrawer(
+        drawerContent = {
+            NewysAppDrawerContent(
+                currentRoute = currentRoute,
+                navigateToHome = navActions.navigateToHome,
+                navigateToSetting = navActions.navigateToSettingsScreen,
+                navigateToFavouriteScreen = navActions.navigateToFavouriteScreen,
+                closeDrawer = {
+                    scope.launch {
+                        drawerState.close()
+                    }
+                }
+            )
+        },
+        drawerState = drawerState
+    ) {
+        NewsyNavigationGraph(
+            navController = navController,
+            navActions = navActions,
+            openDrawer = {
+                scope.launch {
+                    drawerState.open()
+                }
+            }
+        )
+    }
 
-    NewsyNavigationGraph(
-        navController = navController,
-        navActions = navActions,
-        openDrawer = {}
-    )
+
 }
